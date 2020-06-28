fe5655977354f10b1e18111e68baf9b1f8c7a4e3
박경수 <pybsgs1234@gmail.com>
2020-06-28 13:14:06 +0900

    완주하지 못한 선수

---
+++ Algorithm-Programmers/완주하지 못한 선수.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+string solution(vector<string> participant, vector<string> completion);
+
+int main() {
+
+
+	return 0;
+}
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+
+	return answer;
+}
